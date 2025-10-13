4909d5c3a989ec6df0d7252074b7cddc8d841d46
Phoebe Jao <phoebe_jao@commude.ph>
2025-10-13 14:07:22 +0800

    [add] signup form functionality

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,16 +1,24 @@
 import Input from "./Input";
 
 const Form = () => {
-  function handleFormSubmit() {
+  function handleFormSubmit(e) {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+
+    for(let [key, value] of formData.entries()) {
+      console.log({key, value});
+      
+    }
+    
     alert('Sign up successful!');
   }
 
   return (
     <form action="" onSubmit={handleFormSubmit} className="bg-custom-white flex flex-col justify-center items-center px-10 max-sm:px-6 py-10 max-sm:py-6 rounded-[0.8rem]">
-      <Input placeholder="First Name" />
-      <Input placeholder="Last Name" />
-      <Input type="email" placeholder="Email Address" />
-      <Input type="password" placeholder="Password" />
+      <Input name="firstName" placeholder="First Name" />
+      <Input name="lastName" placeholder="Last Name" />
+      <Input type="email" name="email" placeholder="Email Address" />
+      <Input type="password" name="password" placeholder="Password" />
       <button type="submit" className="w-full text-custom-purple-700 bg-custom-yellow font-bold tracking-wide uppercase rounded-[0.5rem] py-4 cursor-pointer hover:opacity-75 transition-all duration-500">Claim your free trial</button>
       <p className="text-[0.7rem] text-custom-purple-700 tracking-wide pt-3 max-sm:pt-5 max-sm:text-center">By clicking the button, you are agreeing to <br className="max-sm:block min-[769px]:hidden" />our <a href="#" className="font-bold">Terms and Conditions</a></p>
     </form>
--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,6 +1,6 @@
-const Input = ({ type="text", placeholder }) => {
+const Input = ({ type="text", name, placeholder }) => {
   return (
-    <input type={type} placeholder={placeholder} required className="w-full border-solid border-[1px] border-custom-purple-500 rounded pl-7 max-sm:pl-5 py-4 mb-5 max-sm:mb-[0.9rem] font-medium outline-none" />
+    <input type={type} name={name} placeholder={placeholder} required className="w-full border-solid border-[1px] border-custom-purple-500 rounded pl-7 max-sm:pl-5 py-4 mb-5 max-sm:mb-[0.9rem] font-medium outline-none" />
   )
 }
 
