9cbb763b115e586be73d1e467a7b5c0e4e821787
Phoebe Jao <phoebe_jao@commude.ph>
2025-10-14 10:02:27 +0800

    [update] heading and description text content

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,9 @@ function App() {
     <main className='bg-gradient-to-b from-custom-purple-500 to-custom-purple-700'>
       <div className="h-screen max-sm:h-full w-full max-w-[69.3rem] max-sm:max-w-[87%] sm:px-6 mx-auto flex justify-between items-center gap-[1.9rem] max-sm:gap-6 max-sm:flex-col max-sm:py-[5.7rem]">
         <div className='w-1/2 max-sm:w-full'>
-          <h1 className='max-sm:max-w-[23rem] text-custom-white text-[3.3rem] max-sm:text-3xl max-sm:text-center max-sm:mx-auto font-bold leading-none max-sm:leading-9 mb-8 max-sm:mb-6'>Learn to code by watching others</h1>
-          <p className='max-sm:max-w-11/12 text-custom-white text-[1.15rem] max-sm:text-[1rem] max-sm:text-justify max-sm:mx-auto  font-medium'>
-            See how experienced developers solve problems in real-time. <br className='min-[48.063rem]:block max-sm:hidden' />
-            Watching scripted tutorials in great, but understanding how developers think is invaluable.
+          <h1 className='max-sm:max-w-[23rem] text-custom-white text-[3.2rem] max-sm:text-3xl max-sm:text-center max-sm:mx-auto font-bold leading-none max-sm:leading-9 mb-8 max-sm:mb-6'>Unlimited Entertainment, <br /><span className='text-custom-yellow'>One Subscription</span></h1>
+          <p className='max-sm:max-w-11/12 text-custom-white text-[1.15rem] max-sm:text-[1rem] text-justify max-sm:mx-auto  font-medium'>
+            Stream movies, shows, and exclusive originals anytime, anywhere. Join now and dive into a world of endless stories— no ads, cancel anytime.
           </p>
         </div>
         <div className='w-1/2 max-sm:w-full'>
